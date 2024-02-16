c26b7bdf0053418b8f5a2ad33ef90ccb5a69563f
Mark Andrey Acebu <markandrey.acebu@gmail.com>
2024-02-16 14:42:19 +0800

    Add comments for url as refernce for each http verb command

--- src/main/kotlin/com/beginnerspring/markseconddemo/httpverbslogback/SecondController.kt
+++ src/main/kotlin/com/beginnerspring/markseconddemo/httpverbslogback/SecondController.kt
@@ -13,21 +13,24 @@ class SecondController @Autowired constructor(private val secondUsersService: Se
     @GetMapping("/secondtry")
     fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) = "Hello $name"
 
+    // localhost:9095/users2?name=kotlin
     @GetMapping("/users2")
     fun getUser(@RequestParam(value = "name") name: String) =
             secondUsersService.getUser(name)
 
-
+    // localhost:9095/users2
     @PostMapping("/users2")
     fun addUser(@RequestBody user: User) =
             secondUsersService.addUser(user)
 
 
+    // localhost:9095/users2?name=mark
     @PutMapping("/users2")
     fun updateUser(@RequestParam(value = "name") name: String, @RequestBody user: User) =
             secondUsersService.updateUser(name, user)
 
 
+    // localhost:9095/users2?name=mark
     @DeleteMapping("/users2")
     fun deleteUser(@RequestParam(value = "name") name: String) =
             secondUsersService.deleteUser(name)
